0a3c158f98e120983603f7be29de4fe4b129a238
unknown <obrienules@gmail.com>
2019-11-29 00:27:43 +0100

    Updates

---
+++ precache-manifest.5ae6d99e5d80d9473cf3578a645e1892.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54dfe57df7c2ca1092a96de1ab614ad4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1eb7dd986ebb8c226c6d",
+    "url": "/monsters-rolodex/static/css/main.c743beff.chunk.css"
+  },
+  {
+    "revision": "1d84f78c1666c89f421d",
+    "url": "/monsters-rolodex/static/js/2.23e46c27.chunk.js"
+  },
+  {
+    "revision": "1eb7dd986ebb8c226c6d",
+    "url": "/monsters-rolodex/static/js/main.da70c802.chunk.js"
+  },
+  {
+    "revision": "1b1f57fd6c4b40aa49d9",
+    "url": "/monsters-rolodex/static/js/runtime-main.10abe5f5.js"
+  }
+]);
